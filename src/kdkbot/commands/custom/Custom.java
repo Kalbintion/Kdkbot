5001820830ccc8fb53bc0cbfe4b76f2317ca458d
Kalbintion <distantimage24@gmail.com>
2014-09-28 15:23:12 -0500

    Custom Commands

--- src/kdkbot/commands/custom/Custom.java
+++ src/kdkbot/commands/custom/Custom.java
@@ -18,18 +18,19 @@ public class Custom implements Command {
 
 	@Override
 	public void executeCommand(String[] args) {
-		
+		// args[0] is channel, args[1] is sender
+		// args[2] is login, args[3] is hostname
+		// args[4] is message
+		instance.sendMessage(args[0], parseParameters(args[0], args[1], args[2], args[3], args[4]));
 	}
 
 	@Override
 	public void setTrigger(String trigger) {
-		// TODO Auto-generated method stub
-		
+		this.trigger = trigger;
 	}
 
 	@Override
 	public String getTrigger() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 

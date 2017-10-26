b48751da1986e815d643d5268179f571fd4a8b62
Kalbintion <Distantimage24@gmail.com>
2017-10-26 09:21:15 -0500

    Update to the quotes page, sorted by # properly for page.

--- web/channels_quotes.php
+++ web/channels_quotes.php
@@ -2,15 +2,24 @@
 	$filPath = "../cfg/#" . $_GET['c'] . "/quotes.cfg";
 	if(file_exists($filPath)) {
 		$fil = file_get_contents($filPath);
-		$quotes = explode("\r\n", $fil);
-		asort($quotes);
+		$quotes_contents = explode("\r\n", $fil);
+		$quotes = array();
+		foreach ($quotes_contents as $quotes_line) {
+			if($quotes_line !== "") {
+			$parts = explode(": ", $quotes_line, 2);
+			$id = $parts[0];
+			$quotes[$id]["id"] = $id;
+			$quotes[$id]["msg"] = $parts[1];
+			}
+		}
+		sort($quotes);
 		
-		echo "<br /><p>Quotes list for " . ucwords(strtolower($_GET['c'])) . "</p>";
+		echo "<br /><h1>Quotes list for " . ucwords(strtolower($_GET['c'])) . "</h1>";
 		echo "<table class=\"minTable\"><tr><th>ID</th><th>Quote Text</th></tr>";
-		foreach ($quotes as $quote) {
-			if($quote !== "") {
-				$quote_segments = explode(": ", $quote, 2);
-				echo "<tr><td>" . $quote_segments[0] . "</td><td>" . $quote_segments[1] . "</td></tr>";
+		
+		foreach ($quotes as $key => $val) {
+			if($key !== "") {
+				echo "<tr><td>" . $val["id"] . "</td><td>" . $val["msg"] . "</td></tr>";
 			}
 		}
 		if(count($quotes) <= 1) { 
